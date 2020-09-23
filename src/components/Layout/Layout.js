53bece4c84701338988f1e2ea9576757553581c9
Hoang Nguyen <hoangnguyenhh@lifung.com>
2020-09-23 15:43:13 +0700

    apply scss

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Aux from '../../hoc/Aux';
+import styles from './Layout.module.scss';
 
 const layout = (props) => (
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={styles.Content}>
             {props.children}
         </main>
     </Aux>
