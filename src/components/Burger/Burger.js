6bf081fe7a140a9a4f94a08092290d188e41a321
Hoang Nguyen <hoangnguyenhh@lifung.com>
2020-09-24 17:04:03 +0700

    apply buildcontrols component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,9 +4,12 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
     const { ingredients } = props;
-    const burgerIngredients = Object.entries(ingredients).map(([key, value], index) => {
+    let burgerIngredients = Object.entries(ingredients).map(([key, value], index) => {
         return [...Array(value)].map((element, index) => <BurgerIngredient type={key} key={key + index} /> )
     }).flatMap(burgerIngredient => burgerIngredient);
+    if(burgerIngredients.length === 0) {
+        burgerIngredients = <p>Please start adding ingredients!</p>
+    }
     return (
         <div className={styles.Burger}>
             <BurgerIngredient type="bread-top" />
