ead47a1ff112b684e2ffb81d136b08b303cc53bd
Hoang Nguyen <hoangnguyenhh@lifung.com>
2020-09-24 14:00:25 +0700

    add burger component

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './Burger.module.scss';
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+
+const burger = (props) => {
+    return (
+        <div className={styles.Burger}>
+            <BurgerIngredient type="bread-top" />
+            <BurgerIngredient type="cheese" />
+            <BurgerIngredient type="meat" />
+            <BurgerIngredient type="bacon" />
+            <BurgerIngredient type="salad" />
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    );
+};
+
+export default burger;
