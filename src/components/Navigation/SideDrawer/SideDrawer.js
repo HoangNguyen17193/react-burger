ccefbf72478962ac44eb9c9adcb5a415de5b1611
Hoang Nguyen <hoangnguyenhh@lifung.com>
2020-09-28 16:43:47 +0700

    add logo container

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,7 +6,9 @@ import classes from './SideDrawer.module.scss';
 const sideDrawer = (props) => {
   return (
       <div className={classes.SideDrawer}>
-          <Logo />
+          <div className={classes.Logo}>
+              <Logo />
+          </div>
           <nav>
               <NavigationItems />
           </nav>
