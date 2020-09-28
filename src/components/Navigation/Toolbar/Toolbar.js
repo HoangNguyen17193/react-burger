ffd3d6c8a617f90873cdeaf65e6439330f16e5e0
Hoang Nguyen <hoangnguyenhh@lifung.com>
2020-09-28 16:13:41 +0700

    apply Navigation Items component

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,14 +1,13 @@
 import React from "react";
 import classes from './Toolbar.module.scss';
 import Logo from '../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
  <header className={classes.Toolbar}>
      <div>MENU</div>
      <Logo />
-     <nav>
-         ...
-     </nav>
+     <NavigationItems />
  </header>
 );
 
