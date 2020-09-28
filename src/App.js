e475c0416cbd6ed9ddfab11ec22c6ea2c8980b3c
Hoang Nguyen <hoangnguyenhh@lifung.com>
2020-09-28 17:03:16 +0700

    remove unused css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 import Layout from "./components/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
