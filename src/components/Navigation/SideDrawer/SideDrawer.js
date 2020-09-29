5e883ccbd50edb4f77bc0ea447ee35c61bd0d2e2
Hoang Nguyen <hoangnguyenhh@lifung.com>
2020-09-29 15:35:36 +0700

    open, close side drawer

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,19 +1,25 @@
 import React from "react";
 import Logo from '../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../Common/Backdrop/Backdrop';
 import classes from './SideDrawer.module.scss';
+import Aux from '../../../hoc/Auxiliary';
 
 const sideDrawer = (props) => {
-  return (
-      <div className={classes.SideDrawer}>
-          <div className={classes.Logo}>
-              <Logo />
-          </div>
-          <nav>
-              <NavigationItems />
-          </nav>
-      </div>
-  );
+    const attachedClasses = [classes.SideDrawer, props.open? classes.Open: classes.Close];
+    return (
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo/>
+                </div>
+                <nav>
+                    <NavigationItems/>
+                </nav>
+            </div>
+        </Aux>
+    );
 };
 
 export default sideDrawer;
