d1b0b498ab1a232e0b1fe7d207aee562b22ab92b
Hoang Nguyen <hoangnguyenhh@lifung.com>
2020-10-01 10:19:49 +0700

    fixed func name

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -12,7 +12,7 @@ const withErrorHandler = (WrappedComponent, axios) => {
             }
         }
 
-        componentWithMount() {
+        componentWillMount() {
             this.requestInterceptor = axios.interceptors.request.use(req => {
                 this.setState({error: null});
                 return req;
