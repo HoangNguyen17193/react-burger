541113cb9881e90461facfaad61ec909c628ee6c
Hoang Nguyen <hoangnguyenhh@lifung.com>
2020-10-05 11:33:41 +0700

    add checkout summary component

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Burger from '../../Burger/Burger';
+import Button from '../../Common/Button/Button';
+import classes from './CheckoutSummary.module.scss';
+
+const checkoutSummary = (props) => {
+    return (
+        <div className={classes.CheckoutSummary}>
+            <h1>We hope it tastes well!</h1>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Burger ingredients={props.ingredients}/>
+            </div>
+            <Button buttonType="Danger" clicked>CANCEL</Button>
+            <Button buttonType="Success" clicked>CONTINUE</Button>
+        </div>
+    );
+};
+
+export default checkoutSummary;
