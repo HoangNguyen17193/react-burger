e15e38f9a35ab3e98326e04b25cafcdf3fdb841d
Hoang Nguyen <hoangnguyenhh@lifung.com>
2020-10-05 12:00:48 +0700

    apply router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Route, Switch} from 'react-router-dom';
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
 
-class App extends Component{
-  render() {
-    return (
-        <Layout>
-           <BurgerBuilder />
-           <Checkout />
-        </Layout>
-    )
-  }
+class App extends Component {
+    render() {
+        return (
+            <Layout>
+                <Switch>
+                    <Route path="/" component={BurgerBuilder}/>
+                    <Route path="/checkout" component={Checkout}/>
+                </Switch>
+            </Layout>
+        )
+    }
 }
 
 export default App;
