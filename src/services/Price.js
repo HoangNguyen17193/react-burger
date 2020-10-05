293c4ee9001865cfaac48969326f966cea640d54
Hoang Nguyen <hoangnguyenhh@lifung.com>
2020-10-05 14:37:23 +0700

    add price service

---
+++ src/services/Price.js
@@ -0,0 +1,15 @@
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+};
+
+export default class Price {
+    static calculateTotalPrice(ingredients) {
+        return Object.entries(ingredients)
+            .map(([key, value]) => INGREDIENT_PRICES[key] * (value || 0))
+            .reduce((sum, price) => sum + price, 0);
+    }
+}
