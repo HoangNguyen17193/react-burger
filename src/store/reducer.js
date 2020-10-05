c60bbc859a88599aa624049497548cfa1feedb82
Hoang Nguyen <hoangnguyenhh@lifung.com>
2020-10-05 16:40:57 +0700

    add actions reducer

---
+++ src/store/reducer.js
@@ -0,0 +1,13 @@
+import {BASE_PRICE} from '../services/Price';
+import * as actionTypes from './actions';
+
+const initialState = {
+    ingredients: null,
+    totalPrice: BASE_PRICE
+};
+
+const reducer = (state = initialState, action) => {
+
+};
+
+export default reducer;
