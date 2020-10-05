14f93de9d673a9452db14fcb4c02a92b680aece5
Hoang Nguyen <hoangnguyenhh@lifung.com>
2020-10-05 14:37:42 +0700

    fixed calculate price

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,21 +6,16 @@ import Modal from '../../components/Common/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/Common/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import Price from "../../services/Price";
 import axios from '../../axios-orders';
 
-const INGREDIENT_PRICES = {
-    salad: 0.5,
-    cheese: 0.4,
-    meat: 1.3,
-    bacon: 0.7
-};
 
 class BurgerBuilder extends Component {
     constructor(props) {
         super(props);
         this.state = {
             ingredients: null,
-            totalPrice: 4,
+            totalPrice: 0,
             purchasable: false,
             purchasing: false,
             loading: false,
@@ -41,11 +36,8 @@ class BurgerBuilder extends Component {
             ...this.state.ingredients
         };
         updatedIngredients[type] = updatedCount;
-        const priceAddition = INGREDIENT_PRICES[type];
-        const oldPrice = this.state.totalPrice;
-        const newPrice =  oldPrice + priceAddition;
         this.setState({
-            totalPrice: newPrice,
+            totalPrice: Price.calculateTotalPrice(updatedIngredients),
             ingredients: updatedIngredients
         });
         this.updatePurchaseState(updatedIngredients);
@@ -61,11 +53,8 @@ class BurgerBuilder extends Component {
             ...this.state.ingredients
         };
         updatedIngredients[type] = updatedCount;
-        const priceAddition = INGREDIENT_PRICES[type];
-        const oldPrice = this.state.totalPrice;
-        const newPrice =  oldPrice - priceAddition;
         this.setState({
-            totalPrice: newPrice,
+            totalPrice: Price.calculateTotalPrice(updatedIngredients),
             ingredients: updatedIngredients
         });
         this.updatePurchaseState(updatedIngredients);
@@ -84,30 +73,6 @@ class BurgerBuilder extends Component {
     };
 
     purchaseContinueHandler = async () => {
-        // this.setState({
-        //     loading: true
-        // });
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Max Schwarzmuller',
-        //         address: {
-        //             street: '',
-        //             zipCode: '',
-        //             country: ''
-        //         }
-        //     },
-        //     email: 'test@test.com'
-        // };
-        // axios.post('/orders.json', order)
-        //     .catch(e => console.log(e))
-        //     .finally(() => {
-        //         this.setState({
-        //             loading: false,
-        //             purchasing: false
-        //         });
-        //     })
         const queryParams = Object.entries(this.state.ingredients)
             .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
             .join('&');
@@ -121,9 +86,9 @@ class BurgerBuilder extends Component {
     componentDidMount() {
         axios.get('/ingredients.json')
             .then(response => {
-                console.log()
                 this.setState({
-                    ingredients: response.data
+                    ingredients: response.data,
+                    totalPrice: Price.calculateTotalPrice(response.data)
                 })
             })
     }
