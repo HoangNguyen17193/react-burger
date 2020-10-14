64d27b39239512793dd153fa606170c128df2748
Hoang Nguyen <hoangnguyenhh@lifung.com>
2020-10-14 15:22:41 +0700

    apply redux saga

--- src/store/burgerBuilder/burgerBuilderAction.js
+++ src/store/burgerBuilder/burgerBuilderAction.js
@@ -1,5 +1,4 @@
 import * as actionTypes from '../actionTypes';
-import axios from '../../axios-orders';
 
 export const addIngredient = (ingredientName) => {
     return {
@@ -30,12 +29,7 @@ export const fetchIngredientsFailed = () => {
 
 
 export const initIngredients = () => {
-    return dispatch => {
-        axios.get('/ingredients.json')
-            .then(response => {
-                dispatch(setIngredients(response.data))
-            }).catch(() => {
-                dispatch(fetchIngredientsFailed())
-            })
+    return {
+        type: actionTypes.INIT_INGREDIENTS
     };
 };
